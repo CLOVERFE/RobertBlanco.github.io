84341b5cf88e48f9de38055bde5fcbf2f2b7ea92
CLOVERFE <elroberuwu@gmail.com>
2025-09-19 00:02:36 -0600

    chore: update docs with rebuilt relative assets

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // use relative paths so assets load correctly from docs/
   plugins: [react()],
 })
